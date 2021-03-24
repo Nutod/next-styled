a6997e8ca9e928c54f9e2c8b9ac9e23d20ceceec
Nutod <alukodotun@gmail.com>
2021-03-25 01:54:16 +0700

    Added critical to global styling

--- styles/global.js
+++ styles/global.js
@@ -1,9 +1,10 @@
 import { createGlobalStyle } from 'styled-components'
 import { reset } from './reset'
 import { elements } from './elements'
+import { critical } from './critical'
 
 export const GlobalStyles = createGlobalStyle`
   ${reset}
-
+  ${critical}
   ${elements}
 `
