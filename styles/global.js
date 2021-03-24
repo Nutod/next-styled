2b3936d3b53bbbeb78915eb786953f5dd80e2844
Nutod <alukodotun@gmail.com>
2021-03-25 01:52:42 +0700

    Added new root styling to critical

--- styles/global.js
+++ styles/global.js
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from 'styled-components'
 import { reset } from './reset'
+import { elements } from './elements'
 
 export const GlobalStyles = createGlobalStyle`
   ${reset}
+
+  ${elements}
 `
